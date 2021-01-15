3af9455e98c478b0ff0d9f8cb8cede0ecdd2d41c
Adam Ponting <rainbowflat@gmail.com>
2021-01-15 15:43:03 +1100

    debug, refactor

--- RocPapSci.jl
+++ RocPapSci.jl
@@ -7,7 +7,7 @@ const COLOUR = [RGB(1,0,0),RGB(0,0.5,0),RGB(0,0,1)]
 # 0= Rock, 1= Paper, 2= Scissors... n beats n-1, n loses to n+1.
 
 # if true, only save the last 10 images after gen 250. Only 6 are needed to make a gif.
-const MAKE_IMAGES_FOR_GIF = false #true
+const MAKE_IMAGES_FOR_GIF = true
 # true -> use Von N neighbourhood, 4 neighbours. false -> Moore neighborhood, 8 neighbours.
 const FOUR_NABES = false 
 
@@ -21,7 +21,7 @@ const CELL_ARRAY_HEI = Integer(IMAGE_HEI/CELL_SIZE)
 const INITIAL_CELLS = rand(0:2, CELL_ARRAY_HEI, CELL_ARRAY_WID)
 const IMAGES = 260
 
-mod1(n,modulus) = mod(n-1,modulus)+1 # given n, returns a number in 1 .. modulus
+mod1(n,modulus) = mod(n-1,modulus)+1  # given n, returns a number in 1 .. modulus
 
 function calculate_winner(x::Integer,y::Integer,cells)
     wins = 0
@@ -29,10 +29,10 @@ function calculate_winner(x::Integer,y::Integer,cells)
     self = cells[y,x]
     for nabe_x in [mod1(x-1, CELL_ARRAY_WID), x, mod1(x+1, CELL_ARRAY_WID)]
         for nabe_y in [mod1(y-1, CELL_ARRAY_HEI), y, mod1(y+1, CELL_ARRAY_HEI)] 
-            if nabe_x == x && nabe_y == y # dont do self cell
+            if nabe_x == x && nabe_y == y  # dont do self cell
                 continue
             elseif FOUR_NABES
-                if nabe_x != x && nabe_y != y # i.e. not on the horiz-vert axis through cell
+                if nabe_x != x && nabe_y != y  # i.e. not on the horiz-vert axis through cell
                     continue
                 end
             end
@@ -73,9 +73,9 @@ function save_images(cells)
         @printf("Doing image %d..\n",image_num)
         cells = [Integer(calculate_winner(x,y,cells)) for y = 1:CELL_ARRAY_HEI::Int, 
             x = 1:CELL_ARRAY_WID::Int]
-        if MAKE_IMAGES_FOR_GIF
-            if image_num>IMAGES-10
-                save_image(image_num-IMAGES+9, cells)
+        if MAKE_IMAGES_FOR_GIF  # save 6 images for the GIF loop
+            if image_num>IMAGES-6
+                save_image(image_num-IMAGES+5, cells)
             end
         else
             save_image(image_num,cells)
